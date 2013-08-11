b5469d903cd0a82ea63f395330a32d129ffa3490
Adam Szczygieł <lypant@tlen.pl>
2013-08-11 17:46:52 +0200

    Next refinements

--- 006_regular_user.sh
+++ 006_regular_user.sh
@@ -1,7 +1,9 @@
 # Add regular user
+echo Adding regular user adam
 useradd -m -g users -G wheel,storage,power -s /bin/bash adam
 
 # Set password
+echo Setting password for adam
 passwd adam
 
 # Add user to sudoers
@@ -9,5 +11,8 @@ visudo
 #adam ALL=(ALL) ALL
 
 # Shut down to do VBox snapshot and log as a regular user
-shutdown -h 0
-
+#shutdown -h 0
+echo User added to sudoers
+echo DONE
+echo Reboot and log in as a regular user. Continue with next instructions.
+ 
---
+++ 007_customisation.sh
@@ -0,0 +1,38 @@
+# Update package list
+sudo pacman -Syy
+
+# Install git
+sudo pacman -S --noconfirm git
+
+# Configure pacman
+# Display total download progress
+sudo sed -i 's/#TotalDownload/TotalDownload/g' /etc/pacman.conf
+
+# Install packets
+sudo pacman -S --noconfirm vim alsa-utils xorg-server xorg-server-utils xorg-xinit mesa virtualbox-guest-utils dmenu rxvt-unicode ttf-inconsolata ttf-dejavu
+
+# Manually load the modules
+sudo modprobe -a vboxguest vboxsf vboxvideo
+
+# Create config file with modules to be loaded
+#sudo cp ~/archconfig/files/virtualbox.conf /etc/modules-load.d/
+sudo cp /etc/archconfig/files/virtualbox.conf /etc/modules-load.d/
+
+# Copy the dotfiles
+#cp ~/archconfig/dotfiles/.* ~
+cp /etc/archconfig/dotfiles/.* ~
+#cp -r ~/archconfig/dotfiles/.vim ~
+cp -r /etc/archconfig/dotfiles/.vim ~
+
+# Get, make and install dwm
+# TODO Use variables for paths
+git clone http://git.suckless.org/dwm ~/build/dwm
+cd ~/build/dwm
+git checkout 4fb31e0 -b my_adjustments
+sed -i 's/PREFIX = \/usr\/local/PREFIX = \/usr/g' ~/build/dwm/config.mk
+sed -i 's/X11INC = \/usr\/X11R6\/include/X11INC = \/usr\/include\/X11/g' ~/build/dwm/config.mk
+sed -i 's/X11LIB = \/usr\/X11R6\/lib/X11LIB = \/usr\/lib\/X11/g' ~/build/dwm/.config.mk
+sed -i 's/"st"/"urxvt"/g' ~/build/dwm/config.def.h
+git commit -a -m "My adjustments"
+sudo make clean install
+
