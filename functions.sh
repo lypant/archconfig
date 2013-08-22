fb13ef0baf831b02430a5847e2f93202ae684dc4
Adam Szczygieł <lypant@tlen.pl>
2013-08-22 21:03:44 +0200

    Add executeScript and anyKey functions

--- functions.sh
+++ functions.sh
@@ -1,16 +1,49 @@
 #!/bin/bash
 
-# Helper functions - taken fron or based on archblocks approach
+# Helper functions - taken from or based on archblocks approach
+
+anyKey ()
+{
+	# Print parameters in new line and wait for user input
+	echo -e "\n$@";
+	read -sn 1 -p "Any key to continue...";
+	echo;
+}
 
 executeScript ()
 {
 	# Do nothing when no parameter is provided
 	[ -z "$@" ] && return
 
-	echo There is some param
-
+	# Process all parameters
 	for script in $@; do
-		echo Script: $script
+		isUrl=false isPath=false isRootPath=false;
+		# Determine the case
+		case "$script" in
+			*://*) isUrl=true;;
+			/*)    isRootPath=true;;
+			*/*)   isPath=true;;	
+		esac
+		file="${script/%.sh/}.sh";
+		if $isUrl; then
+			url="${file}";
+		elif [ -f "${DIR/%\//}/${file}" ]; then
+			url="file://${file}";
+		else
+			url="${REPOSITORY/%\//}/${file}";
+		fi
+
+		# Load the script
+		loadedScript="$(curl -fsL ${url})";
+		
+		# Execute the script
+		[ -n "$loadedScript" ] && eval "${loadedScript}";
+
+		# Check for errors
+		if [ "$?" -gt 0 ]; then
+			# Display warning
+			anyKey "Execution of script \"$script\" experienced errors"
+		fi
 	done
 }
 
