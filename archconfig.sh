e0a850866297cbc177fe08dd73e3ec9bfa4550ea
Adam Szczygieł <lypant@tlen.pl>
2013-08-23 19:02:28 +0200

    Continuation of experiment

--- archconfig.sh
+++ archconfig.sh
@@ -4,3 +4,7 @@ REPOSITORY="https://raw.github.com/lypant/archconfig/experiments"
 
 curl -o "functions.sh" -fsL "$REPOSITORY/functions.sh"
 
+source functions.sh
+
+downloadFile "$REPOSITORY/some_script.sh" "some_script.sh"
+
