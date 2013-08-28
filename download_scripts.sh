b25bfc7c03dba95438118845764d9b27604ef755
Adam Szczygieł <lypant@tlen.pl>
2013-08-28 19:24:00 +0200

    Added missing script to be downloaded

--- download_scripts.sh
+++ download_scripts.sh
@@ -9,4 +9,5 @@ downloadFile "$REPOSITORY/install.sh" "$DEST_DIR/install.sh"
 downloadFile "$REPOSITORY/partition_disk.sh" "$DEST_DIR/partition_disk.sh"
 downloadFile "$REPOSITORY/base_system.sh" "$DEST_DIR/base_system.sh"
 downloadFile "$REPOSITORY/chrooted.sh" "$DEST_DIR/chrooted.sh"
+downloadFile "$REPOSITORY/post_chroot.sh" "$DEST_DIR/post_chroot.sh"
 
