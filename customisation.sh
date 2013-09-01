eb0e6ff132e7ed6baa75d132d13b736eee1c7a7a
Adam Szczygieł <lypant@tlen.pl>
2013-09-01 16:09:06 +0200

    Fix directory ownership

--- customisation.sh
+++ customisation.sh
@@ -36,7 +36,7 @@ chown "adam:users" "/etc/modules-load.d/virtualbox.conf"
 chown "adam:users" "/home/adam/.bash_profile"
 chown "adam:users" "/home/adam/.bashrc"
 chown "adam:users" "/home/adam/.gitconfig"
-chown "adam:users" "/home/adam/.vim"
+chown -R "adam:users" "/home/adam/.vim"
 chown "adam:users" "/home/adam/.vim/colors/solarized.vim"
 chown "adam:users" "/home/adam/.vimrc"
 chown "adam:users" "/home/adam/.Xdefaults"
@@ -47,7 +47,6 @@ chown "adam:users" "/home/adam/.xinitrc"
 echo "Installing dwm"
 DWM_BUILD_PATH="/home/adam/build/dwm"
 git clone http://git.suckless.org/dwm "$DWM_BUILD_PATH"
-chown "adam:users" "$DWM_BUILD_PATH"
 cd $DWM_BUILD_PATH
 git checkout 4fb31e0 -b my_adjustments
 sed -i 's/PREFIX = \/usr\/local/PREFIX = \/usr/g' "$DWM_BUILD_PATH/config.mk"
@@ -56,6 +55,7 @@ sed -i 's/X11LIB = \/usr\/X11R6\/lib/X11LIB = \/usr\/lib\/X11/g' "$DWM_BUILD_PAT
 sed -i 's/"st"/"urxvt"/g' "$DWM_BUILD_PATH/config.def.h"
 git commit -a -m "My adjustments"
 make clean install
+chown -R "adam:users" "$DWM_BUILD_PATH"
  
 # Custom video mode for terminal (1680x1050) - requires VBox setup at the beginning
 # Quiet mode - no logs during bootup
