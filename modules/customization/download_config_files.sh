07d9b331ddc09f20ceb457d945977876be08ea1d
Adam Szczygieł <lypant@tlen.pl>
2013-09-15 16:37:41 +0200

    Fix config files paths

--- modules/customization/download_config_files.sh
+++ modules/customization/download_config_files.sh
@@ -2,14 +2,14 @@
 
 # TODO: Download all files in download.sh; here just copy them
 
-downloadFileSudo "$REPOSITORY/files/virtualbox.conf" "/etc/modules-load.d/virtualbox.conf" "--overwrite"
-downloadFileSudo "$REPOSITORY/files/issue" "/etc/issue" "--overwrite"
+downloadFileSudo "$REPOSITORY/modules/files/virtualbox.conf" "/etc/modules-load.d/virtualbox.conf" "--overwrite"
+downloadFileSudo "$REPOSITORY/modules/files/issue" "/etc/issue" "--overwrite"
 
-downloadFile "$REPOSITORY/files/dotfiles/.bash_profile" "/home/adam/.bash_profile" "--overwrite"
-downloadFile "$REPOSITORY/files/dotfiles/.bashrc" "/home/adam/.bashrc" "--overwrite"
-downloadFile "$REPOSITORY/files/dotfiles/.gitconfig" "/home/adam/.gitconfig" "--overwrite"
-downloadFile "$REPOSITORY/files/dotfiles/.vim/colors/solarized.vim" "/home/adam/.vim/colors/solarized.vim" "--overwrite"
-downloadFile "$REPOSITORY/files/dotfiles/.vimrc" "/home/adam/.vimrc" "--overwrite"
-downloadFile "$REPOSITORY/files/dotfiles/.Xdefaults" "/home/adam/.Xdefaults" "--overwrite"
-downloadFile "$REPOSITORY/files/dotfiles/.xinitrc" "/home/adam/.xinitrc" "--overwrite"
+downloadFile "$REPOSITORY/modules/files/dotfiles/.bash_profile" "/home/adam/.bash_profile" "--overwrite"
+downloadFile "$REPOSITORY/modules/files/dotfiles/.bashrc" "/home/adam/.bashrc" "--overwrite"
+downloadFile "$REPOSITORY/modules/files/dotfiles/.gitconfig" "/home/adam/.gitconfig" "--overwrite"
+downloadFile "$REPOSITORY/modules/files/dotfiles/.vim/colors/solarized.vim" "/home/adam/.vim/colors/solarized.vim" "--overwrite"
+downloadFile "$REPOSITORY/modules/files/dotfiles/.vimrc" "/home/adam/.vimrc" "--overwrite"
+downloadFile "$REPOSITORY/modules/files/dotfiles/.Xdefaults" "/home/adam/.Xdefaults" "--overwrite"
+downloadFile "$REPOSITORY/modules/files/dotfiles/.xinitrc" "/home/adam/.xinitrc" "--overwrite"
 
