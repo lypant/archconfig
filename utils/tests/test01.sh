bcad15b5dfd33020201708e457eef25dbc4a15c6
Adam Szczygieł <lypant@tlen.pl>
2013-09-19 21:18:31 +0200

    Add new functions with logging. Add tests for them.

---
+++ utils/tests/test01.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#source ../new_functions.sh
+source ../new_functions.sh output.log
+
+#executeCommand
+#executeCommand pwd
+#executeCommand rm non_existing_file
+
+#sth pwd
+#sth "rm blablabla"
+#sth "rm skasuj.to"
+
+#anyKey
+#anyKey Raz dwa trzy
+#anyKey "Paused"
+
+#downloadFile "https://raw.github.com/lypant/archconfig/modular/get.sh" "get.sh"
+#downloadFile "https://raw.github.com/lypant/archconfig/modular/get.sh" "get.sh" "--overwrite"
+#downloadFile "https://raw.github.com/lypant/archconfig/modular/nonexisting.file" "void"
+
+
+#commentVar "X" "settings.fake"
+#commentVarSudo "X" "settings.fake"
+#echo "Result of commentX: $?"
+
+#uncommentVar "X" "settings.fake"
+#uncommentVarSudo "X" "settings.fake"
+#echo "Result of uncommentX: $?"
+
